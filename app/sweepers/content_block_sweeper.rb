5634ce84fa7c968e1cc8f3db2f8e2760c2f70c3e
Stuart Bates <stuart@builtbyclick.com>
2014-04-26 11:37:58 +0100

    Force update

--- app/sweepers/content_block_sweeper.rb
+++ app/sweepers/content_block_sweeper.rb
@@ -3,6 +3,7 @@ class ContentBlockSweeper < ActionController::Caching::Sweeper
   observe Spree::ContentBlock
 
   def sweep(content_block)
+    p "Clearing cache for content_block_#{content_block.token}"
     expire_fragment("content_block_#{content_block.token}")
   end
   alias_method :after_create, :sweep
